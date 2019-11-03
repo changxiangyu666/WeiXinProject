301946737e14afe8ca2060704f1e6cca01458ae6
changxiangyu <1792286994@qq.com>
2019-11-03 15:45:02 +0800

    微信app图书小项目

---
+++ wxapp-little-shelf/server/conf/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+    appConfig: {
+        appid: 'wx07c699d88a2c11dc',
+        secret: 'chang190996'
+    },
+    userConfig: {
+        credit: 30          // 用户初始积分余额
+    }
+}
---
+++ wxapp-little-shelf/server/conf/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '123456',
+        database: 'wxapp',
+        port: 3306  // your database server port, default is 3306
+    }
+}
